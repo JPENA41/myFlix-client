cb5c8fe6d5bdea5cc7a58b05675950416dcc0425
JPENA41 <jpenavillalba7348@gmail.com>
2021-11-13 13:51:52 -0700

    Import Prtoptypes & SCSS import

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// SCSS Import
+import "./movie-view.scss"
 
 export class MovieView extends React.Component {
 
@@ -60,3 +64,24 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.date,
+      Death: PropTypes.date
+    }),
+    Actors: PropTypes.array.isRequired,
+    Featured: PropTypes.bool,
+    ImagePath: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
